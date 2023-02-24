e3fe6816e815707363e67cd5312e7d195c0ba823
joão Victor <joaoeusoueu13225@gmail.com>
2023-02-24 20:06:25 -0300

    mais um

---
+++ Aulas/A-3/scripts.js
@@ -0,0 +1,20 @@
+console.log("primeiro");
+
+console.log("ultimo");
+
+console.log ("mais um console.log");
+
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
+
+//este é um comentario
+
+/*e
+esse
+um
+de
+multiplas
+linhas*/
